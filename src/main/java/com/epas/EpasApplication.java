6c146ddfdd810b83fde87fb90002052d10910594
Harshad Kumbhar <harshadkumbhar66@gmail.com>
2026-07-05 12:10:01 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/epas/EpasApplication.java
@@ -0,0 +1,13 @@
+package com.epas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpasApplication.class, args);
+	}
+
+}
