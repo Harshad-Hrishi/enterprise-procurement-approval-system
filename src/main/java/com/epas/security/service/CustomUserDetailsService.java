133c4d12361162d0b918dc703d28a4fb34e45676
Harshad Kumbhar <harshadkumbhar66@gmail.com>
2026-07-05 21:23:05 +0530

    feat(security): complete authentication and role-based login

--- src/main/java/com/epas/security/service/CustomUserDetailsService.java
+++ src/main/java/com/epas/security/service/CustomUserDetailsService.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
 import java.util.List;
@@ -24,6 +25,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRoleRepository userRoleRepository;
     private final RolePermissionRepository rolePermissionRepository;
 
+    @Transactional(readOnly = true)
     @Override
     public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
